9a06d652134b43beec1ca4ee82e7d7d180dd62bd
Warren Harper II <warrenharper2@att.net>
2019-11-12 21:24:20 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,7 +1,7 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 hash = {:blake => 500, :ashley => 2, :adam => 1}
-def key_for_min_value(name_hash)
+
   def key_for_min_value(name_hash)
     lowest_value = 0
     lowest_key = nil
